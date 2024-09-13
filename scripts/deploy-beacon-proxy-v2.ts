f7834f517caae5520154f1666af8da8aa16a388a
Antonio <tonioufa@gmail.com>
2024-09-13 15:24:45 +0100

    fix upgrade scripts

--- scripts/deploy-beacon-proxy-v2.ts
+++ scripts/deploy-beacon-proxy-v2.ts
@@ -2,10 +2,9 @@
 async function main() {
   console.log(`Upgrading beacon proxy contract in ${hre.network.name}`);
     // const PROXY_ADDRESS = "0xf47ccc15A0e7003fb800Db0Ce552d23818Ee6C75" // Sepolia
-  const PROXY_ADDRESS = "0x26066575A64511f295AA6b7c1dA7db7059b9523f" // ZKsync
+  const PROXY_ADDRESS = "0xE6EedAeb287Ad5f04C5F3515F988CFfbFdBec7c2" // ZKsync
   const proxyFactory = await hre.ethers.getContractFactory("V2_Beacon");
-  const fundingGoalInWei = hre.ethers.parseEther('0.1').toString();
-  const campaignContract_v2 = await hre.upgrades.upgradeBeacon(PROXY_ADDRESS, proxyFactory, [fundingGoalInWei]);
+  const campaignContract_v2 = await hre.upgrades.upgradeBeacon(PROXY_ADDRESS, proxyFactory);
   await campaignContract_v2.waitForDeployment();
 
   const contractAddress = await campaignContract_v2.getAddress();
--- scripts/deploy-uups-proxy-v2.ts
+++ scripts/deploy-uups-proxy-v2.ts
@@ -1,19 +1,17 @@
 
 async function main() {
   console.log(`Upgrading UUPS proxy contract on ${hre.network.name}`);
-//   const PROXY_ADDRESS = "0xb846C065C9865f6F02DC3ac496F4877ec010eD8a" // ZKsync
-  const PROXY_ADDRESS = "0xc6ECb2Edea7e343bda7D8c76e5b1e86528facaf0" // Sepolia
+  const PROXY_ADDRESS = "0xE6EedAeb287Ad5f04C5F3515F988CFfbFdBec7c2" // ZKsync
+  // const PROXY_ADDRESS = "0xc6ECb2Edea7e343bda7D8c76e5b1e86528facaf0" // Sepolia
   const proxyFactory = await hre.ethers.getContractFactory("V2_UUPS");
-  const fundingGoalInWei = hre.ethers.parseEther('0.1').toString();
-  const campaignContract_v2 = await hre.upgrades.upgradeProxy(PROXY_ADDRESS, proxyFactory, [fundingGoalInWei]);
+  const campaignContract_v2 = await hre.upgrades.upgradeProxy(PROXY_ADDRESS, proxyFactory);
   await campaignContract_v2.waitForDeployment();
 
   const contractAddress = await campaignContract_v2.getAddress();
   console.log(`UUPS proxy upgraded on ${contractAddress}`);
 
-  await hre.run("verify", {
+  await hre.run("verify:verify", {
       address: contractAddress,
-      constructorArguments: [fundingGoalInWei],
   });
 }
 
